5e8c0207b41a075a812962ca1db27cdbb52a4946
Moataz Basyouni <mb.motaz@yahoo.com>
2020-01-25 20:48:55 -0800

    finished hw 4

---
+++ ExtractBodyM.sh
@@ -0,0 +1,3 @@
+cp ~/Developer/repos/eeb-c177-homework/class_assignments/class-work-Tuesday-1-21/BodyM.csv ~/Developer/repos/eeb-c177-scripts/NewBodyM.csv
+
+#I made the script run automatically using #!/bin/bash then gave it permission with schmod +x Extract BodyM.sh then made it run using ./ExtractBodyM.sh
